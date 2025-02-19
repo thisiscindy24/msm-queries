4dc6ac1290a5028a1c901bb441accd538d5a7e26
thisiscindy24 <cindyannwu@gmail.com>
2025-02-19 21:42:29 +0000

    direct details high level

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,11 @@ class DirectorsController < ApplicationController
   def index
     render ({:template=>"directors_templates/list"})
   end
+
+  def show
+    the_id = params.fetch("the_id")
+    matching_records = Director.where({:id => the_id})
+    @the_director = matching_records.at(0)
+    render ({:template=>"directors_templates/details"})
+  end
 end
