dde70fa656c68fed6daa15b563a5977f548059a0
thisiscindy24 <cindyannwu@gmail.com>
2025-02-20 06:54:21 +0000

    youngest

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -9,4 +9,10 @@ class DirectorsController < ApplicationController
     @the_director = matching_records.at(0)
     render ({:template=>"directors_templates/details"})
   end
+
+  def young
+    @youngest_director = Director.order(dob: :desc).first
+    
+    render ({:template=>"directors_templates/young"})
+  end
 end
