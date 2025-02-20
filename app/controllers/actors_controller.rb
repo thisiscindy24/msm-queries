c0cd7e49e21e6cf2b0963c4bc4c4d8754505dd03
thisiscindy24 <cindyannwu@gmail.com>
2025-02-20 06:01:36 +0000

    actors

---
+++ app/controllers/actors_controller.rb
@@ -0,0 +1,12 @@
+class ActorsController < ApplicationController
+  def index
+    render ({:template=>"actors_templates/list"})
+  end
+
+  def show
+    the_id = params.fetch("the_id")
+    matching_records = Actor.where({:id => the_id})
+    @the_actor = matching_records.at(0)
+    render ({:template=>"actors_templates/details"})
+  end
+end
